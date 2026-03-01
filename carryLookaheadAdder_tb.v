956deb874bf6644f7f03950178f919ae2eea0603
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-01 09:58:51 -0500

    implemented testbench logic

---
+++ carryLookaheadAdder_tb.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module carryLookaheadAdder_tb();
+    
+    reg[3:0] A,B;
+    reg Cin;
+    
+    wire S3,S2,S1,S0,Co;
+    
+    carryLookaheadAdder uut(A,B,Cin,S3,S22,S1,S0,Co);
+    integer k,j;
+    
+    initial begin
+        for(k=0;k<16;k=k+1)begin
+            for(j=0;j<16;j=j+1)begin
+                A = k;
+                B = j;
+                Cin = 1'b0;
+                #10 $display("A = %b, B=%b, Cin = 0, S = %b, Co = %b", A,B,Cin,{S3,S2,S1,S0},Co);
+            end
+        end
+    end
+    
+endmodule
